aeb41248cfc5359726583c4adf8ffd8234f53f80
jackl-3 <blstrmn@outlook.com>
2025-08-08 07:52:07 -0700

    pages edit: 2025-08-08 07:52:06

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -17,7 +17,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+//    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -32,13 +32,13 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-//    Component.Backlinks(),
+    Component.Backlinks(),
   ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.ArticleTitle(), Component.ContentMeta()], // removed "Component.Breadcrumbs(),"
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
